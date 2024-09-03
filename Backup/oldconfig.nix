53ddb2cfc3f76865ab9a7d21156347bd8cf3153b
Kou-ding <fotispapadak@gmail.com>
2024-09-03 04:21:46 +0300

    nix conf

--- Backup/oldconfig.nix
+++ Backup/oldconfig.nix
@@ -1,4 +1,4 @@
-## Edit this configuration file to define what should be installed on
+# Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
@@ -10,8 +10,10 @@
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
+  ############ Bootloader ############
+  # Default Bootloader
   #boot.loader.systemd-boot.enable = true;
+  # Grub Bootloader
   boot.loader.grub = {
     enable = true; 
     device = "nodev";
@@ -19,15 +21,71 @@
     useOSProber = true;
   };
   boot.loader.efi.canTouchEfiVariables = true;
+ 
+  ####### Login manager ########
+  # Enables the X11 windowing system
+  # Since we are using Hyprland WM which uses wayland 
+  # we don't have to enable x11.
+  services.xserver.enable = true;
+  # Minimal login screen that launches Hyprland on start up
+  # Hyprland executes hyprlock and make it our login screen
+  services.greetd = {
+    enable = true;
+    restart = false;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        user = "kou";
+      };
+      initial_session = {
+        command = "Hyprland";
+        user = "kou";
+      };
+    };
+  }; 
 
-  # Hostname
+  ######### Lock screen ##########
+  # Hyperlock
+  programs.hyprlock.enable = true;
+
+  ######## Window Manager ########
+  # Hyprland
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  ########### Fonts ############
+  # While the fonts were installed in the system packages list 
+  # they have to be here too for the system apps to be able to utilize them.
+  # Fonts help render certain icons, emojis, text, etc...
+  fonts.packages = with pkgs; [
+    nerdfonts
+  ];
+  
+  ########## Theme #########
+  qt.enable = true;
+  qt.platformTheme = "gnome";
+  qt.style = "adwaita-dark";
+  # Force Adwaita Dark for GNOME applications
+  environment.sessionVariables.GTK_THEME = "Adwaita:dark";
+
+  # Desktop portals handle program interactions, screensharing, link opening, file opening etc...
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
+ 
+  ######## Basic configuration ########
+  # The same stuff someone would choose inside an installer
+  # hostname
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Enables wireless support via wpa_supplicant.
+  # networking.wireless.enable = true; 
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
+  
   # Enable networking
   networking.networkmanager.enable = true;
 
@@ -38,124 +96,33 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "C.UTF-8";
-    LC_IDENTIFICATION = "C.UTF-8";
-    LC_MEASUREMENT = "C.UTF-8";
-    LC_MONETARY = "C.UTF-8";
-    LC_NAME = "C.UTF-8";
-    LC_NUMERIC = "C.UTF-8";
-    LC_PAPER = "C.UTF-8";
-    LC_TELEPHONE = "C.UTF-8";
-    LC_TIME = "C.UTF-8";
+    LC_ADDRESS = "el_GR.UTF-8";
+    LC_IDENTIFICATION = "el_GR.UTF-8";
+    LC_MEASUREMENT = "el_GR.UTF-8";
+    LC_MONETARY = "el_GR.UTF-8";
+    LC_NAME = "el_GR.UTF-8";
+    LC_NUMERIC = "el_GR.UTF-8";
+    LC_PAPER = "el_GR.UTF-8";
+    LC_TELEPHONE = "el_GR.UTF-8";
+    LC_TIME = "el_GR.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  #services.xserver.displayManager.gdm.enable = true;
-  #services.xserver.desktopManager.gnome.enable = true;
-  
-  # Enable the KDE Desktop Environment
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  #environment.plasma6.excludePackages = with pkgs.kdePackages; [
-  #  plasma-browser-integration
-  #  oxygen
-  #  konsole
-  #];
-
-  ####### Login manager ########
-  # Minimal login screen that launches Hyprland on start up
-  # Hyprland executes hyprlock and make it our login screen
-  #services.greetd = {
-  #  enable = true;
-  #  restart = false;
-  #  settings = {
-  #    default_session = {
-  #      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
-  #      user = "kou";
-  #    };
-  #    initial_session = {
-  #      command = "Hyprland";
-  #      user = "kou";
-  #    };
-  #  };
-  #}; 
-  
-  ######## Window Manager ########
-  # Hyprland
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };   
-  # Desktop portals handle program interactions, screensharing, link opening, file opening etc...
-  #xdg.portal.enable = true;
-  #xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
-
-  ######### Lock screen ##########
-  # Hyperlock
-  programs.hyprlock.enable = true;
-
-  ########## Theme #########
-  qt = {
-    enable = true;
-    platformTheme = "gnome";
-    style = "adwaita-dark";
-  };
-  # Force Adwaita Dark for GNOME applications
-  #environment.sessionVariables.GTK_THEME = "Adwaita:dark";
-  
-  ########### Fonts ############
-  # While the fonts were installed in the system packages list 
-  # they have to be here too for the system apps to be able to utilize them.
-  # Fonts help render certain icons, emojis, text, etc...
-  fonts.packages = with pkgs; [
-    nerdfonts
-  ];
-  
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
+  ############### User ##################
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.kou = {
     isNormalUser = true;
     description = "kou";
     shell = pkgs.bash;
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
+    extraGroups = [ "networkmanager" "video" "input" "wheel" ];
+    packages = with pkgs; [];
   };
 
-  # Install firefox.
-  #programs.firefox.enable = true;
-
+  ################## Packages #####################
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
@@ -164,10 +131,8 @@
   environment.systemPackages = with pkgs; [
     # Basic Applications 
     firefox # Browser
-    thunderbird # Mail 
-    #gnome.nautilus # gnome file explorer
-    #xfce.thunar # xfce file explorer
-    #swappy # Image Viewer
+    xfce.thunar # File explorer
+    swappy # Image Viewer
 
     # Optional Applications
     obsidian # Markdown notes
@@ -182,7 +147,7 @@
 
     # Ricing
     hyprland # windows manager
-    #hyprpaper # wallpaper deamon
+    hyprpaper # wallpaper deamon
     hyprlock # lock screen
     rofi-wayland # app launcher
     waybar # top bar
@@ -192,12 +157,12 @@
     neovim # editor
     nerdfonts # fonts
     bibata-cursors # cursor theme
-    #adwaita-qt # theme
-    #gtk3  # For Adwaita theme
-    #gnome.adwaita-icon-theme # The Adwaita theme icons 
-    #gsettings-desktop-schemas # settings for gnome applications
-    #greetd.greetd # simple login manager
-    #greetd.tuigreet # simple login manager
+    adwaita-qt # theme
+    gtk3  # For Adwaita theme
+    adwaita-icon-theme # The Adwaita theme icons 
+    gsettings-desktop-schemas # settings for gnome applications
+    greetd.greetd # simple login manager
+    greetd.tuigreet # simple login manager
     nwg-look # gtk theme picker, mainly for selecting a cursor out of all the downloaded ones
     
     # Fun packages 
@@ -214,14 +179,13 @@
     wget # download from command line
     git # version control
     dunst # notification deamon, dunst is x11 + wayland, mako is pure wayland
-    #pulseaudio # paplay to play a notification sound
     wl-clipboard # clipboard
     grim # screenshot dep 1
     slurp # screenshot dep 2
     libnotify # dependency of notification deamon
     os-prober # for dual booting
-    #libsForQt5.qt5.qtquickcontrols2 # sddm dep 1
-    #libsForQt5.qt5.qtgraphicaleffects # sddm dep 2
+    libsForQt5.qt5.qtquickcontrols2 # sddm dep 1
+    libsForQt5.qt5.qtgraphicaleffects # sddm dep 2
     gptfdisk # gpt disk tool
     gcc14 # C programming language
     bc # synth shell dependency
@@ -237,6 +201,19 @@
     })
   ];
 
+  ############# Sound #############
+  # Enable sound with pipewire
+  # sound.enable = true; # no longer needed
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable  = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+  
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -245,17 +222,18 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
+  ########## Services ##########
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
+  services.openssh.enable = true;
+  
+  ######### Firewall ##########
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
+  
+  ######################### NIXOS VERSION ##########################
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -263,5 +241,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
-
 }
